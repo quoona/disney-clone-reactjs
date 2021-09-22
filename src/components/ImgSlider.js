d58d2bbca91a0646cfd62e7863c54a30bc593c56
quoona <nqnam229@gmail.com>
2021-09-22 12:47:00 +0700

    ADD: Viewers

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -61,8 +61,8 @@ const Wrap = styled.div`
     width: 100%;
     height: 100%;
     border-radius: 5px;
-    box-shadow: rgb(0 0 0 /69%) 0px 26px 3-px -10px,
-      rgb (0 0 0 /73%) 0px 16px 10px -10px;
+    box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+      rgb(0 0 0 /73%) 0px 16px 10px -10px;
     cursor: pointer;
     transition: 300ms;
 
